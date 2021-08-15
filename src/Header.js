57fae9bacfc1a12011c625fc2f9beb7360d91cf1
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-08-15 18:02:41 +0300

    aa

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
-import './Header.css';
+import "./Header.css";
+import LogoutButton from "./components/LogoutButton";
+import LoginButton from "./components/LoginButton";
 
 class Header extends React.Component {
   render() {
-    return(
+    return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
         <Link to="/">Home</Link>
         <Link to="/profile">Profile</Link>
         {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
+        {!this.props.data.isAuthenticated ? <LoginButton /> : <LogoutButton />}
       </Navbar>
     );
   }
